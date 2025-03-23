e1770e1c34f901c03134838c6f8c7a0ae44e1fa0
Zet <mati.zygadlo@gmail.com>
2025-03-23 21:03:02 +0100

    Dodane puszczanie testów z ustawionymi parametrami

--- CVRP_ACO/MainClass.cs
+++ CVRP_ACO/MainClass.cs
@@ -34,7 +34,7 @@ class MainClass
             Console.WriteLine("\n----------------------------");
             Console.WriteLine("1. Wczytaj z pliku\n2. Wyświetl dane\n3. Algorytm mrówkowy");
             Console.WriteLine($"4. Liczba mrówek ({ants})\n5. Współczynnik parowania RHO ({rho})\n6. Stała feromonowa ({q})");
-            Console.WriteLine($"7. Maksymalny czas algorytmu ({maxTimeACO} s)\n8. Softmax\n9. KNN \n10. KNN 2 OPT \n0. Zakończ");
+            Console.WriteLine($"7. Maksymalny czas algorytmu ({maxTimeACO} s)\n8. Softmax\n9. KNN \n10.KNN 2 OPT \n11.Single Core Mrówkowy\n12.Algorytm mrówkowy z optymalizacją parametrów\n0. Zakończ");
             int choice = int.Parse(Console.ReadLine());
 
             switch (choice)
@@ -135,9 +135,53 @@ class MainClass
                         Console.WriteLine($"Czas wykonania algorytmu: {stopwatch.ElapsedMilliseconds} ms");
                     }
                     break;
-                case 0:
-                    return;
+                case 11:
+
+
+                     string[] filenames5 = new string[] {"A-n44-k6.txt", "A-n55-k9.txt", "A-n63-k10.txt","A-n69-k9.txt", "A-n80-k10.txt"  };
+                    foreach (var filename in filenames5)
+                    {
+                        Console.WriteLine("\n" + filename);
+                        var cvrp = CVRPInstance.LoadFromFile(filename);
+                        cvrp.createDistanceMatrix(cvrp.Nodes);
+                        Console.WriteLine("Ant Colony Single Core");
+                        Stopwatch stopwatch = new Stopwatch();
+
+                        stopwatch.Start();
+                        antColony.AntColonyOptimization(cvrp, alpha, beta, rho, q, maxIterations, maxTimeACO, out bestSolution, out bestCost);
+
+                        stopwatch.Stop();
+                        Console.WriteLine($"Czas wykonania algorytmu: {stopwatch.ElapsedMilliseconds} ms");
+                    }
                     break;
+                case 12:
+                var testCases = new[]
+                {
+                    new { Filename = "A-n44-k6.txt", Alpha = 1.1, Beta = 1.7, Rho = 1.0, Q = 1.0 },
+                    new { Filename = "A-n55-k9.txt", Alpha = 2.4, Beta = 2.6, Rho = 0.95, Q = 200.0 },
+                    new { Filename = "A-n63-k10.txt", Alpha = 1.8, Beta = 1.2, Rho = 0.65, Q = 50.0 },
+                    new { Filename = "A-n69-k9.txt", Alpha = 1.4, Beta = 1.9, Rho = 0.9, Q = 100.0 },
+                    new { Filename = "A-n80-k10.txt", Alpha = 1.9, Beta = 1.5, Rho = 1.0, Q = 200.0 }
+                };
+
+                foreach (var testCase in testCases)
+                {
+                    Console.WriteLine("\n" + testCase.Filename);
+                    var cvrp = CVRPInstance.LoadFromFile(testCase.Filename);
+                    cvrp.createDistanceMatrix(cvrp.Nodes);
+
+                    Console.WriteLine("Algorytm mrówkowy z optymalizacją parametrów");
+
+                    Stopwatch stopwatch = new Stopwatch();
+
+                    stopwatch.Start();
+                    antColony.AntColonyOptimizationPararrelv2(cvrp, testCase.Alpha, testCase.Beta, testCase.Rho, testCase.Q, maxIterations, maxTimeACO, out bestSolution, out bestCost);
+                    stopwatch.Stop();
+
+                    Console.WriteLine($"\nCzas wykonania algorytmu wielowątkowego: {stopwatch.ElapsedMilliseconds} ms");
+                }
+                    break;
+                
                 default:
                     Console.WriteLine("Nieprawidłowa opcja.");
                     break;
